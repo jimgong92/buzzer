11d1fbc04cbf48e7694d300f46bda7f146c0da01
Jimmy Gong <gong.jim@gmail.com>
2016-02-24 19:31:15 -0800

    Updates gitignore, initial buzzer setup, closes #1, closes #2, closes #3

---
+++ index.js
@@ -0,0 +1,57 @@
+var schedule = require('node-schedule');
+var request = require('superagent');
+
+/**
+ * Buzzer Config
+ * - interval (number as ms) - default to 20 minutes
+ * - startHour (date) - default to 7am
+ * - startMinute (date) - default to 30
+ * - endHour (date) - default to 12am
+ * - endMinute (date) - default to 40
+ * - endpoint (string)
+ * - callback (fn) - OPTIONAL to be invoked when the ping comes back
+ *                 - Takes in error and response 
+ */
+function Buzzer(config) {
+  this._interval = config.interval || (1000 * 60 * 20);
+  this._startHour = config.startHour || 7;
+  this._startMinute = config.startMinute || 30;
+  this._endHour = config.endHour || 12;
+  this._endMinute = config.endMinute || 40;
+  this._buzzSchedule = null;
+  if (!this._endpoint) {
+    throw new Error('No endpoint specified');
+  }
+}
+
+Buzzer.prototype = {
+  buzz: function() {
+    request
+      .get(this._endpoint)
+      .end(function(err, res) {
+        if (this._callback !== undefined) {
+          this._callback(err, res);
+        }
+      });
+  },
+  activate: function() {
+    var rule = new schedule.RecurrenceRule();
+    rule.hour = this._startHour;
+    rule.minute = this._startMinute;
+    var j = schedule.scheduleJob(rule, function() {
+      this.buzz();
+      this._buzzSchedule = setInterval(this.buzz, this._interval);
+    });
+  },
+  deactivate: function() {
+    var rule = new schedule.RecurrenceRule();
+    rule.hour = this._endHour;
+    rule.minute = this._endMinute;
+    var j = schedule.scheduleJob(rule, function() {
+      clearInterval(this._buzzSchedule);
+      this._buzzSchedule = null;
+    });
+  }
+};
+
+module.exports = Buzzer;
---
+++ test/index.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var Buzzer = require('../index');
+
+var buzzerConfig = {
+
+};
+
+var buzzer = new Buzzer(buzzerConfig);
+
+describe('Buzzer', function() {
+  xit('should have default values', function(done) {
+    done();
+  });
+  xit('should be passed an endpoint to ping', function(done) {
+    done();
+  });
+  xit('should accept a callback to process the ping response', function(done) {
+    done();
+  });
+  xit('should activate manually (upon invoking activate method)', function(done) {
+    done();
+  });
+  xit('should deactivate upon invoking deactivate method', function(done) {
+    done();
+  });
+});
